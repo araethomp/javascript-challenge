753ad076f77afa755435bbcebb9e4d20b0f4c8e5
araethomp <angela.r.thomp@gmail.com>
2021-04-02 15:18:18 -0700

    created cells for data

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,22 @@
 // from data.js
-var tableData = data;
+const tableData = data;
+console.log(tableData);
 
-// YOUR CODE HERE!
+// // Grab table from index
+const tbody = d3.select("tbody");
+console.log(tbody);
+
+tableData.forEach(function(ufoEntry) {
+   console.log(ufoEntry);
+   // Create rows for each ufoEntry and add each entry
+   let row = tbody.append("tr");
+
+   Object.entries(ufoEntry).forEach(function([key, value]){
+     //console.log(key, value);
+
+     entryCell = row.append("td");
+     //console.log(entryCell);
+     entryCell.text(value);
+
+   })
+})
